ee2d59144e9e8b21f022537d450d7e2877d00dc3
timStankevych <tim.stankevich@gmail.com>
2020-08-31 18:38:32 +0300

    Updates

---
+++ precache-manifest.9419b94265b323079598b52ce0265a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20bb93756c3d26fbf65200d4c9668d0b",
+    "url": "/SocialLink_tsx/index.html"
+  },
+  {
+    "revision": "b5442f664e4f8a1afbc1",
+    "url": "/SocialLink_tsx/static/css/main.3f176e95.chunk.css"
+  },
+  {
+    "revision": "53a0b330930d117e6785",
+    "url": "/SocialLink_tsx/static/js/2.3b925dac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialLink_tsx/static/js/2.3b925dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5442f664e4f8a1afbc1",
+    "url": "/SocialLink_tsx/static/js/main.aa6ec5ad.chunk.js"
+  },
+  {
+    "revision": "4508ecf32c185919e951",
+    "url": "/SocialLink_tsx/static/js/runtime-main.87ecf5f6.js"
+  }
+]);
