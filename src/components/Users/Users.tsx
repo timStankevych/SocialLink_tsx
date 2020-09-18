7296cc638d71d6dc9f508f056a66021b885a2bc2
timStankevych <tim.stankevich@gmail.com>
2020-09-18 14:02:12 +0300

    added axios, first get

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import cl from './Users.module.css';
 import {UsersType} from '../../redux/usersReducer';
+import * as axios from 'axios';
+import {debuglog} from 'util';
 
 type PropsType = {
     users: UsersType[]
@@ -12,29 +14,36 @@ type PropsType = {
 let Users = (props: PropsType) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                followed: true,
-                fullName: 'Tymofii',
-                status: 'hi bro',
-                location: {city: 'Bratske', country: 'Ukraine'}
-            },
-            {
-                id: 2,
-                followed: false,
-                fullName: 'Fima',
-                status: 'hi bro',
-                location: {city: 'Bratske', country: 'Ukraine'}
-            },
-            {
-                id: 3,
-                followed: true,
-                fullName: 'Mary',
-                status: 'hi bro',
-                location: {city: 'Bratske', country: 'Ukraine'}
-            },
-        ]);
+
+        axios.default.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                    props.setUsers(response.data.items);
+                }
+            );
+
+        // props.setUsers([
+        //     {
+        //         id: 1,
+        //         followed: true,
+        //         fullName: 'Tymofii',
+        //         status: 'hi bro',
+        //         location: {city: 'Bratske', country: 'Ukraine'}
+        //     },
+        //     {
+        //         id: 2,
+        //         followed: false,
+        //         fullName: 'Fima',
+        //         status: 'hi bro',
+        //         location: {city: 'Bratske', country: 'Ukraine'}
+        //     },
+        //     {
+        //         id: 3,
+        //         followed: true,
+        //         fullName: 'Mary',
+        //         status: 'hi bro',
+        //         location: {city: 'Bratske', country: 'Ukraine'}
+        //     },
+        // ]);
     }
 
     return (<div>
@@ -43,7 +52,7 @@ let Users = (props: PropsType) => {
                 <span>
                     <div>
                         <img className={cl.ava}
-                             src="https://i.insider.com/5ebf0f16aee6a826327d9111?width=1100&format=jpeg&auto=webp"
+                             src={u.photos.small != null ? u.photos.small : 'https://pm1.narvii.com/6889/74979d4d2744ec6e27995b6e866f091d04c0b40cr1-515-414v2_hq.jpg'}
                              alt="avatar"/>
                     </div>
                     <div>
@@ -58,12 +67,12 @@ let Users = (props: PropsType) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>)
--- src/redux/usersReducer.ts
+++ src/redux/usersReducer.ts
@@ -1,19 +1,30 @@
 import {ActionTypes} from './redux-store';
 
-export type UsersPageActionTypes = ReturnType<typeof followAC>
-| ReturnType<typeof unFollowAC>
-| ReturnType<typeof setUsersAC>
 
-type LocationType = {
-    city: string
-    country: string
-}
- export type UsersType = {
-    id: number
+export type UsersPageActionTypes = ReturnType<typeof followAC>
+    | ReturnType<typeof unFollowAC>
+    | ReturnType<typeof setUsersAC>
+
+// type LocationType = {
+//     city: string
+//     country: string
+// }
+// export type UsersType = {
+//     id: number
+//     followed: boolean
+//     fullName: string
+//     status: string
+//     photos: any
+//     location: LocationType
+// }
+
+export type UsersType = {
     followed: boolean
-    fullName: string
-    status: string
-    location: LocationType
+    id: number
+    name: string
+    photos: any
+    status: any
+    uniqueUrlName: any
 }
 
 export type usersPageType = {
@@ -25,9 +36,9 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 
-export const followAC = (userId: number) => ({type: FOLLOW, userId})as const;
-export const unFollowAC = (userId: number) => ({type: UNFOLLOW, userId})as const;
-export const setUsersAC = (users: Array<UsersType>) => ({type: SET_USERS, users})as const;
+export const followAC = (userId: number) => ({type: FOLLOW, userId}) as const;
+export const unFollowAC = (userId: number) => ({type: UNFOLLOW, userId}) as const;
+export const setUsersAC = (users: Array<UsersType>) => ({type: SET_USERS, users}) as const;
 
 let initialState = {
     users: [],
@@ -40,7 +51,7 @@ const usersReducer = (state: usersPageType = initialState, action: ActionTypes) 
                 ...state,
                 users: state.users.map(u => {
                     if (u.id === action.userId) {
-                        return {...u, followed: false}
+                        return {...u, followed: false};
                     }
                     return u;
                 })
@@ -50,7 +61,7 @@ const usersReducer = (state: usersPageType = initialState, action: ActionTypes) 
                 ...state,
                 users: state.users.map(u => {
                     if (u.id === action.userId) {
-                        return {...u, followed: true}
+                        return {...u, followed: true};
                     }
                     return u;
                 })
@@ -64,3 +75,4 @@ const usersReducer = (state: usersPageType = initialState, action: ActionTypes) 
 };
 
 export default usersReducer;
+
